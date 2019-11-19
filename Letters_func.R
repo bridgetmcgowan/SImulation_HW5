f0ee702308a5d8d839f2b8556d8237c916ed6b29
bridgetmcgowan <bridget.mcgowan1@gmail.com>
2019-11-19 13:44:46 -0600

    Updates to HW 5

--- Letters_func.R
+++ Letters_func.R
@@ -1,9 +1,13 @@
-
+library(installr)
 library(devtools)
-
+library(pkgbuild)
+#library(Rtools)
+install.packages("Rtools")
 alph<-function(x){
   sample(letters,1)
 }
+
+
 
 package.skeleton(list = "alph",name = "returnALetter")
 
